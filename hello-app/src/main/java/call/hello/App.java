c524411a0ad30359e42de8e5086c226a121243c9
Fatih TEPE, Ed.D <79118602+fatihtepe@users.noreply.github.com>
2021-10-05 22:44:03 -0400

    Update App.java

--- hello-app/src/main/java/call/hello/App.java
+++ hello-app/src/main/java/call/hello/App.java
@@ -3,7 +3,7 @@ package call.hello;
 public class App
 {
 
-    private final String message = "Hello World from Java Maven Project!";
+    private final String message = "Hello World from Java Maven Project!Tarik!!";
 
     public App() {}
 
